f145cc5afc6a24c345d06600102c3b5c32872ae1
rogeritael <rogeritael.contato@gmail.com>
2022-04-27 17:13:59 -0300

    rota de exclusão de notas

--- api/app/routes/notes.js
+++ api/app/routes/notes.js
@@ -2,6 +2,8 @@ let express = require('express');
 let router = express.Router();
 const Note = require('../models/note');
 const withAuth = require('../middlewares/auth');
+const { findOneAndDelete, findById } = require('../models/note');
+const note = require('../models/note');
 
 router.post('/', withAuth, async (req, res) => {
     const {title, body} = req.body;
@@ -60,6 +62,22 @@ router.put('/:id', withAuth, async (req, res) => {
     }
 });
 
+
+router.delete('/:id', withAuth, async(req, res) => {
+    const {id} = req.params;
+    try {
+        let note = await Note.findById(id);
+        if(isOwner(req.user, note)){
+            await note.delete();
+            res.json({message: "deleted"}).status(204);
+        }else{
+            res.status(403).json({error: "permission denied"});
+        };
+
+    } catch (error) {
+        res.status(500).json({error: "problem to delete a note"});
+    }
+});
 
 
 const isOwner = (user, note) => {
